70eaef94ffc6bb7f3955645c2841f1dc6019d730
Loren <lm.velasquez12@gmail.com>
2025-01-20 16:24:54 -0600

    remove registry links

--- src/views/RegistryView.vue
+++ src/views/RegistryView.vue
@@ -3,19 +3,7 @@
     <ul>
       <li>
         <a class="hover:text-slate-900" href="https://www.amazon.com" target="_blank" rel="noopener"
-          >Amazon Registry</a
-        >
-      </li>
-
-      <li>
-        <a class="hover:text-slate-900" href="https://www.target.com" target="_blank" rel="noopener"
-          >Target Registry</a
-        >
-      </li>
-
-      <li>
-        <a class="hover:text-slate-900" href="https://www.costco.com" target="_blank" rel="noopener"
-          >Costco Registry</a
+          >Coming Soon!</a
         >
       </li>
     </ul>
