902e49c872ebadc3787428ff624463e02e750652
Hector Rios <that.hector@gmail.com>
2025-04-05 09:25:06 -0500

    feat: Registry + minor UI fixes

--- src/views/RegistryView.vue
+++ src/views/RegistryView.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="registry">
-    <ul>
-      <li>
-        <a class="hover:text-slate-900" href="https://www.amazon.com" target="_blank" rel="noopener"
-          >Coming Soon!</a
-        >
-      </li>
-    </ul>
+    <h1 class="red text-3xl/10">Where's the registry?</h1>
+    <p class="event pl-1">It's available on <a href="https://www.honeyfund.com/site/rios-velasquez-05-02-2025" target="_blank">Honeyfund</a>!</p>
+
+    <br>
+
+    <h1 class="red text-3xl/10">What do we want?</h1>
+    <p class="event pl-1">
+    We don't really need much in the form of house gifts, but we figured that
+    we could ask you to help make our honeymoon unforgettable.
+    </p>
+
+    <br>
+
+    <p class="event pl-1">
+    We found <a href="https://www.honeyfund.com/site/rios-velasquez-05-02-2025"
+                target="_blank">Honeyfund</a> so that we can give you a glimpse
+              of what we want to do for our honeymoon with the funds we receive
+              from the wedding. Honeyfund does take a minor portion (~3%), so
+              if you want us to use your whole gift, you can Zelle us at one of
+              our phone numbers.
+    </p>
   </div>
 </template>
--- src/views/ScheduleView.vue
+++ src/views/ScheduleView.vue
@@ -2,18 +2,18 @@
 import ScheduleItem from '@/components/ScheduleItem.vue'
 
 const dayScheduleItems = [
-  { time: '11am', event: 'Doors Open' },
-  { time: '11:15am', event: 'Ceremony' },
-  { time: '11:45am', event: 'Posed pictures/Drinks' },
-  { time: '12:15pm', event: 'Food' },
-  { time: '1pm', event: 'Live Music' },
-  { time: '1:30pm', event: 'Dessert, Toast & Bye' }
+  { time: '11 am', event: 'Doors Open' },
+  { time: '11:15 am', event: 'Ceremony' },
+  { time: '11:45 am', event: 'Posed pictures/Drinks' },
+  { time: '12:15 pm', event: 'Food' },
+  { time: '1 pm', event: 'Live Music' },
+  { time: '1:30 pm', event: 'Dessert, Toast & Bye' }
 ]
 </script>
 
 <template>
   <div class="schedule">
-    <h1 class="red text-2xl/10">Day Schedule:</h1>
+    <h1 class="red text-3xl/10">Day Schedule</h1>
     <ScheduleItem
       v-for="item in dayScheduleItems"
       :time="item.time"
@@ -22,10 +22,10 @@ const dayScheduleItems = [
     />
     <br>
     <br>
-    <h1 class="red text-2xl/10">Night Schedule:</h1>
+    <h1 class="red text-3xl/10">Night Schedule</h1>
     <ScheduleItem
-      time='6pm'
-      event='Drinks'
+      time='6 pm'
+      event='Drinks & Revelry'
     />
   </div>
 </template>
